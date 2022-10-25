cef130d3e8e34b6e9b8b1c34bd7037777760e153
Олег Капранов <arg0nny@mail.ru>
2022-10-25 22:30:21 +0700

    PasscodeLock 1.4.0
    Fixed typo in changelog

--- PasscodeLock/PasscodeLock.plugin.js
+++ PasscodeLock/PasscodeLock.plugin.js
@@ -36,7 +36,7 @@ module.exports = (() => {
                 ]
             },
             {
-                "type": "improvements",
+                "type": "improved",
                 "title": "Improvements",
                 "items": [
                     "Auto-lock setting is more flexible now."
